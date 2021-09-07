526d50fdc70d101dac4cad60b996cd3adee1448d
Roberto <y.roberto@ymail.com>
2021-09-07 08:28:23 -0300

    implemented post retrieval by similarity

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from taggit.models import Tag
 
 from .models import Post, Comment
@@ -24,7 +25,7 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-
+    
     context = {'page':page, 'posts':posts, 'tag':tag}
 
     return render(request,'blog/post/list.html', context)
@@ -48,7 +49,12 @@ def post_detail(request, year, month, day, post):
     else:
         form=CommentForm()
 
-    context = {'post': post, 'form':form, 'new_comment':new_comment, 'comments':comments}
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    published_posts = Post.objects.filter(status='published')
+    similar_posts = published_posts.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    context = {'post': post, 'form':form, 'new_comment':new_comment, 'comments':comments, 'similar_posts':similar_posts}
     return render(request,'blog/post/detail.html', context)
 
 
